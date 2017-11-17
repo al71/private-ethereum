beb3370b36eddec08752ae835635dab06fd4c067
al71 <33250938+al71@users.noreply.github.com>
2017-11-17 15:30:43 +0300

    Create quorum-truffle.js

---
+++ quorum-truffle.js
@@ -0,0 +1,49 @@
+module.exports = {
+  networks: {
+    node11: {
+      host: "141.105.65.227",
+      port: 22000, // was 8545
+      network_id: "*" // Match any network id
+    },
+    node21:  {
+      host: "141.105.65.228",
+      port: 22000,
+      network_id: "*" // Match any network id
+    },
+    node31:  {
+      host: "141.105.65.229",
+      port: 22000,
+      network_id: "*" // Match any network id
+    },
+    node14:  {
+      host: "141.105.65.227",
+      port: 22003,
+      network_id: "*" // Match any network id
+    },
+    node15:  {
+      host: "141.105.65.227",
+      port: 22004,
+      network_id: "*" // Match any network id
+    },
+    node24:  {
+      host: "141.105.65.228",
+      port: 22003,
+      network_id: "*" // Match any network id
+    },
+    node25:  {
+      host: "141.105.65.228",
+      port: 22004,
+      network_id: "*" // Match any network id
+    },
+    node34:  {
+      host: "141.105.65.229",
+      port: 22003,
+      network_id: "*" // Match any network id
+    },
+    node35:  {
+      host: "141.105.65.229",
+      port: 22004,
+      network_id: "*" // Match any network id
+    },	
+  }
+};
