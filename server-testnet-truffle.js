d9de76cd6f6f2ebd5689ecc4035e5a55fe8204ee
al71 <33250938+al71@users.noreply.github.com>
2017-11-20 15:03:13 +0300

    Update server-testnet-truffle.js

--- server-testnet-truffle.js
+++ server-testnet-truffle.js
@@ -2,7 +2,7 @@ module.exports = {
   networks: {
     "testnet": {
       network_id: 3,
-      host: "localhost",
+      host: "141.105.65.234",
       port: 8545  // Different than the default below
     }
   },
