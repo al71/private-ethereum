fba702d597811de2573647aa89f62d8b64371886
al71 <33250938+al71@users.noreply.github.com>
2017-11-20 15:02:06 +0300

    Create server-testnet-truffle.js

---
+++ server-testnet-truffle.js
@@ -0,0 +1,13 @@
+module.exports = {
+  networks: {
+    "testnet": {
+      network_id: 3,
+      host: "localhost",
+      port: 8545  // Different than the default below
+    }
+  },
+  rpc: {
+    host: "localhost",
+    port: 8545
+  }
+};
